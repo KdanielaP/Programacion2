0420af99702f9055cebd6494e6324bdca80dd36b
KdanielaP <135910567+KdanielaP@users.noreply.github.com>
2024-07-20 20:44:01 -0600

    first commit

---
+++ src/gt/edu/miumg/progra2/Personaje.java
@@ -0,0 +1,11 @@
+package gt.edu.miumg.progra2;
+
+public class Personaje {
+    private string nombre ;
+    private string descripcion ;
+    private long tamaño;
+    private int poder ;
+    private int vida ;
+
+
+}
